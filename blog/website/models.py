df835b6e969dc7d58d2658d52af15643e5c6d31f
unknown <anukovich2@gmail.com>
2024-11-06 23:57:58 +0100

    fixed wrapping long words (v1 - simple)

--- blog/website/models.py
+++ blog/website/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class BlogItems(models.Model):
     user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
